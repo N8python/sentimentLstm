564961622079121735743524ab46dd7beee9a49c
N8 <n8@n8programs.com>
2021-01-14 14:03:17 -0500

    add sentiment loading screen

--- public/main.js
+++ public/main.js
@@ -1,6 +1,7 @@
 const toAnalyze = document.getElementById("toAnalyze");
 const sentimentP = document.getElementById("sentiment");
 const analyzeMessage = async() => {
+    sentimentP.innerHTML = `Sentiment: Calculating...`;
     $.post("/analyze-message", {
         message: toAnalyze.value
     }, (data) => {
